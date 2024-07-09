17f6c124e009462c2e565458ecba8be97dc866d3
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-07-09 18:43:31 -0400

    blog post model

--- database/models/blog_post.py
+++ database/models/blog_post.py
@@ -9,7 +9,35 @@ Edit Log:
 """
 
 # STANDARD LIBRARY IMPORTS
+from datetime import datetime
+from sqlalchemy import String, Boolean, DateTime, Text
+from sqlalchemy.orm import mapped_column, Mapped
 
 # THIRD PARTY LIBRARY IMPORTS
 
 # LOCAL LIBRARY IMPORTS
+from database.database import BASE
+from src.models.blog_post_model import BlogPostModel
+
+
+class BlogPost(BASE):
+    """
+    A model for the blog post table
+    """
+
+    __tablename__ = "blog_posts"
+
+    post_name: Mapped[str] = mapped_column(
+        String(256), primary_key=True, unique=True, nullable=False
+    )
+    description: Mapped[str] = mapped_column(String(50), nullable=False)
+    text: Mapped[str] = mapped_column(Text, nullable=False)
+    released: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    release_date: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    last_updated: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
+
+    def __init__(self, blog_post: BlogPostModel):
+        self.post_name = blog_post.post_name
+        self.description = blog_post.description
+        self.text = blog_post.text
+        self.released = blog_post.released
---
+++ src/models/blog_post_model.py
@@ -0,0 +1,33 @@
+"""
+file_name = blog_post_model.py
+Created On: 2024/07/09
+Lasted Updated: 2024/07/09
+Description: _FILL OUT HERE_
+Edit Log:
+2024/07/09
+    - Created file
+"""
+
+# STANDARD LIBRARY IMPORTS
+
+# THIRD PARTY LIBRARY IMPORTS
+from pydantic import BaseModel, Field
+
+# LOCAL LIBRARY IMPORTS
+
+
+class BlogPostModel(BaseModel):
+    """
+    A pydantic model for the blog post
+    """
+
+    post_name: str = Field(
+        ..., max_length=256, description="The name of the blog post, unique"
+    )
+    description: str = Field(
+        ..., max_length=50, description="A short description of the blog post"
+    )
+    text: str = Field(..., description="The full text of the blog post")
+    released: bool = Field(
+        default=False, description="Flag indicating if the blog post is released"
+    )
