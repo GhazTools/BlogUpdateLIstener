494268660bdc06be6c3cc760184dfd777adff510
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-07-10 02:22:31 -0400

    image repository

--- database/repositories/image_repository.py
+++ database/repositories/image_repository.py
@@ -15,6 +15,7 @@ from sqlalchemy.orm import Session
 
 # LOCAL LIBRARY IMPORTS
 from database.database import SESSION_MAKER
+from database.models.image import Image
 
 
 class ImageRepository:
@@ -50,3 +51,73 @@ class ImageRepository:
         """
 
         self.session.close()
+
+    def insert_image(self: "ImageRepository", image: Image) -> None:
+        """
+        Insert a new image into the database.
+
+        Args:
+            image: The image to insert.
+        """
+
+        if self.check_if_exists(image.image_name):
+            raise ValueError("Image already exists")
+
+        self.session.add(image)
+        self.session.commit()
+
+    def check_if_exists(self: "ImageRepository", image_name: str) -> bool:
+        """
+        Check if an image with the given name exists in the database.
+
+        Args:
+            image_name: The name of the image to check for.
+
+        Returns:
+            True if the image exists, False otherwise.
+        """
+
+        return (
+            self.session.query(Image).filter(Image.image_name == image_name).count() > 0
+        )
+
+    def check_if_released(self: "ImageRepository", image_name: str) -> bool:
+        """
+        Check if an image with the given name has been released.
+
+        Args:
+            image_name: The name of the image to check for.
+
+        Returns:
+            True if the image has been released, False otherwise.
+        """
+
+        return (
+            self.session.query(Image)
+            .filter(Image.image_name == image_name, Image.released)
+            .count()
+            > 0
+        )
+
+    def update_release(self: "ImageRepository", image_name: str, release: bool) -> bool:
+        """
+        Update the release state of an image
+
+        Args:
+            image_name: The name of the image to update release for.
+
+        Returns:
+            True if the image release attribute was updated, False otherwise.
+        """
+
+        image: Image | None = (
+            self.session.query(Image).filter(Image.image_name == image_name).first()
+        )
+
+        if image is None:
+            raise ValueError(f"Image not found: {image_name}")
+
+        image.released = release
+        self.session.commit()
+
+        return True
