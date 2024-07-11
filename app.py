73f84ce0153314ff7f10557e324d9b70d1d2c5db
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2024-07-11 00:44:48 -0400

    cleanup

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ from sanic import Sanic
 
 # LOCAL LIBRARY IMPORTS
 from src.app_instantiator import AppInstantiator
-from src.database.repositories.image_repository import ImageRepository
 
 
 APP_INSTANTIATOR = AppInstantiator()
@@ -23,13 +22,3 @@ app: Sanic = APP_INSTANTIATOR.app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
-    # vault_reader = app.config["VAULT_READER"]
-    # print(vault_reader)
-
-    # images = vault_reader.images_to_add
-
-    # print(images)
-
-    # with ImageRepository() as image_repo:
-    #     for image in images:
-    #         image_repo.insert_image(image)
